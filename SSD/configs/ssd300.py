63b5d544b97b93d628b6bd144f861c4250b528e5
Torjesn <torjesn@stud.ntnu.no>
2022-03-25 01:27:26 +0100

    4c

--- SSD/configs/ssd300.py
+++ SSD/configs/ssd300.py
@@ -10,7 +10,7 @@ from ssd.data.mnist import MNISTDetectionDataset
 from .utils import get_dataset_dir, get_output_dir
 
 train = dict(
-    batch_size=32,
+    batch_size=64,
     amp=True, # Automatic mixed precision
     log_interval=20,
     seed=0,
@@ -49,7 +49,7 @@ model = L(SSD300)(
 
 optimizer = L(torch.optim.Adam)(
     # Tip: Scale the learning rate by batch size! 2.6e-3 is set for a batch size of 32. use 2*2.6e-3 if you use 64
-    lr=5e-4,  weight_decay=0.0005,# momentum=0.9,
+    lr=2*2.6e-3,  weight_decay=0.0005,# momentum=0.9,
 )
 schedulers = dict(
     linear=L(LinearLR)(start_factor=0.1, end_factor=1, total_iters=500),
